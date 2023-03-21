30ef452bdeb8242c2e294482c7eec7f7d20be01e
Aayushi Sinha <aayushi.sinha1980@gmail.com>
2023-03-21 22:44:06 +0530

    add about section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,15 @@ module.exports = {
       colors:{
         'bg-color': '#11011e',
       },
+      animation: {
+        'spin-slow': 'spin 5s linear infinite',
+      }
     },
     fontFamily:{
       lato: ['Lato', 'sans-serif'],
       roboto: ['Roboto', 'sans-serif'],
     },
+
   },
   plugins: [],
 }
