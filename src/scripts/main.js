7435c294f15822286749f562be8a0880a656da49
Aayushi Sinha <aayushi.sinha1980@gmail.com>
2023-03-30 05:12:53 +0530

    Changed nav-menu and added responsive about-section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,7 @@ const menuIcon = document.getElementById('menu');
 const navLinks = document.querySelector('.navlinks')
 menuIcon.onclick = ()=>{
     menuIcon.name= menuIcon.name==="menu-outline" ? "close-outline":"menu-outline";
-    navLinks.classList.toggle("top-[80%]")
+    navLinks.classList.toggle("left-[0%]")
 }
 
 
