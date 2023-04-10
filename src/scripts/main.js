7bc2b9f829bcaafc71806b6e67007c9301e79d30
Nancy <ynancy030@gmail.com>
2023-04-11 01:36:38 +0530

    added javascript for testimonials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,28 @@ menuIcon.onclick = ()=>{
 }
 
 
+
+
+
+// testimonials section 
+
+const content = document.querySelectorAll('.moreContent');
+const fullContent = document.querySelectorAll('.fullContent');
+const testimonialBtn = document.querySelector('#testimonialBtn');
+
+testimonialBtn.addEventListener('click' , () =>{
+    if(testimonialBtn.innerText === "Show more"){
+    content.forEach((contents) => {
+        contents.classList.remove('hidden');
+      });
+    testimonialBtn.innerText = "Show less";
+}
+
+    else{
+        content.forEach((contents) => {
+            contents.classList.add('hidden');
+          });
+
+        testimonialBtn.innerText = "Show more";
+    }
+});
