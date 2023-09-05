c292bcf364acc719d928e1f422ed8e5fdaf0617c
Harsh Soni <22bcs046@iiitdwd.ac.in>
2023-09-05 23:45:04 +0530

    added api functionality for members

--- src/scripts/components/members.js
+++ src/scripts/components/members.js
@@ -71,7 +71,7 @@ async function loadMembers(role) {
         const data = await response.json();
         let membersAppended = [];
         
-        if(role === undefined)
+        if(role === undefined || role === 'All')
         {
             membersAppended = [];
             for (let roleMembers in data) {
@@ -108,35 +108,53 @@ async function loadMembers(role) {
 
 function main()
 {
-    const isMediumWindow = window.innerWidth < 992;
-    if(!isMediumWindow)
-    {
-        const nonTechMembers = document.getElementById("nonTechMembers");
-        const webDevMembers = document.getElementById("webDevMembers");
-        const appDevMembers = document.getElementById("appDevMembers");
-        const techLeadMembers = document.getElementById("techLeadMembers");
-        const selectMembers = document.getElementById("selectMembers");
-        const heading = document.getElementById("heading");
-    
-        heading.addEventListener("click", ()=>{loadMembers()});
-        techLeadMembers.addEventListener("click", ()=>{loadMembers("Tech")});
-        webDevMembers.addEventListener("click", ()=>{loadMembers("webDev")});
-        appDevMembers.addEventListener("click", ()=>{loadMembers("appDev")});
-        nonTechMembers.addEventListener("click", ()=>{loadMembers("nonTech")});
-    }
-    else
-    {
+    loadMembers()
+    .then(()=>{
+        // const isMediumWindow = window.innerWidth < 768;
+        const member_roles = document.querySelectorAll(".member-button");
+        
+        let chosen_button = "";
+
+        member_roles.forEach((button)=>{
+            button.onclick = ()=>{
+                member_roles.forEach((btn)=>{
+                    btn.classList = "member-button relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group cursor-pointer";
+                    btn.children[0].classList = "w-48 h-48 rounded rotate-[-40deg] bg-purple-600 absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0";
+                    btn.children[1].classList = "relative w-full text-left text-black transition-colors duration-300 ease-in-out group-hover:text-white";
+                });
+                if(button.id === chosen_button)
+                {
+                    loadMembers();
+                    chosen_button = "";
+                }
+                else
+                {
+                    loadMembers(button.id);
+                    chosen_button = button.id;
+                    button.classList.remove("bg-white");
+                    button.classList.add("bg-purple-600");
+                    button.classList.remove("hover:bg-white");
+                    button.children[0].classList.remove("bg-purple-600");
+                    button.children[0].classList.add("bg-white-700");
+                    button.children[1].classList.remove("text-black");
+                    button.children[1].classList.add("text-white");
+                    button.children[1].classList.remove("group-hover:text-white");
+                    button.children[1].classList.add("group-hover:text-black");
+                }
+            };
+        });
+
         const selectMembers = document.getElementById("selectMembers");
         selectMembers.addEventListener("change", (event)=>{
             loadMembers(event.target.value);
         })
-    };
+    });
 }
 
 export default function membersjs() {
-// members Section
-loadMembers().then(()=>
-{
-    main();
-});
+    // members Section
+    loadMembers().then(()=>
+    {
+        main();
+    });
 }
