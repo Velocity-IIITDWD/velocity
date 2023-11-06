e9292f9696a967aae7791081447c2a5e4e216e99
Anish <anish888999@gmail.com>
2023-11-06 14:22:42 +0530

    refactored home

--- src/scripts/components/headerJs.js
+++ src/scripts/components/headerJs.js
@@ -1,13 +1,11 @@
 export default function headerJs() {
-  
-  const menuIcon = document.getElementById('menu')
-  const navLinks = document.querySelector('.navlinks')
+  const menuIcon = document.getElementById('menu');
+  const navLinks = document.querySelector('.navlinks');
   menuIcon.onclick = () => {
-    menuIcon.name =
-      menuIcon.name === 'menu-outline' ? 'close-outline' : 'menu-outline'
-    navLinks.classList.toggle('left-[0%]')
-    menuIcon.classList.toggle('fixed')
-    menuIcon.classList.toggle('right-[10%]')
-    menuIcon.classList.toggle('z-[100]')
-  }
+    menuIcon.name = menuIcon.name === 'menu-outline' ? 'close-outline' : 'menu-outline';
+    navLinks.classList.toggle('left-[0%]');
+    menuIcon.classList.toggle('fixed');
+    menuIcon.classList.toggle('right-[10%]');
+    menuIcon.classList.toggle('z-[100]');
+  };
 }
