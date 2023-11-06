cc75488bb97559702b6b9922b92991f4a73a7f68
Anish <anish888999@gmail.com>
2023-11-06 14:26:00 +0530

    refactored modules

--- src/scripts/components/modules.js
+++ src/scripts/components/modules.js
@@ -1,6 +1,4 @@
-
 export default function modulejs() {
-
   //Modules Section Card Carousel
   function moduleCarousel() {
     var swiper = new window.Swiper('.card_slider',{
@@ -26,27 +24,31 @@ export default function modulejs() {
   });
   }
 
-  // fetch api
   function fetchData() {
+    // Fetch modules from API
     fetch('./api/modules.json')
       .then((response) => response.json())
       .then((data) => {
           const mainDiv = document.getElementById('blogCard');
 
+          // Add each module in response to DOM
           for(const p of data){
             const iframee = document.createElement('div');
             iframee.classList = "relative rounded-md swiper-slide h-full w-full max-w-2xl border-none";
             mainDiv.appendChild(iframee);
 
+            // Add image to iframe
             const img = document.createElement("img");
             img.classList = "rounded-md h-full w-full";
             img.src = p.img;
             iframee.appendChild(img);
             
+            // Add text to module
             const textDiv = document.createElement("div");
             textDiv.classList = "ease-in-out duration-150 absolute rounded-md opacity-0 bg-transparent hover:opacity-95 hover:backdrop-blur-sm hover:bg-purple-300/60 inset-0 flex flex-col items-center justify-center w-full h-full";
             iframee.appendChild(textDiv);
 
+            // Add button
             const button = document.createElement("a");
             button.type = "button";
             button.classList = "text-slate-750 bg-blue-600 rounded-md w-auto p-2"
@@ -58,6 +60,7 @@ export default function modulejs() {
       })
   }
 
+  // Fetch data and then initialize carousel
   fetchData();
   setTimeout(moduleCarousel , 1000);
 }
